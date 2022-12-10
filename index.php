ce45083818b123c75921f180a4452ce448060fe4
Patryk Hegenberg <hegenberg@tixeltec.com>
2022-12-10 13:25:38 +0100

    phpTutorial: github repo synced.

--- index.php
+++ index.php
@@ -4,15 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Demo</title>
 </head>
 <body>
+    <?php 
+        $name = "Dark Matter";
+        $read = true;
+        if ($read) {
+            $message = "You have read $name";
+        }
+        else {
+            $message = "You havn't read $name.";
+        }
+    ?>
     <h1>
-        <?php 
-        $greeting = "Hello";
-        echo $greeting . ", " . "Everybody!";
-        echo "$greeting, Everybody!";
-        ?>
+        <?php echo $message?>
     </h1>
 </body>
 </html>
