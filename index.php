57abb48e63e96d942fec467704ecf0a980802b24
Patryk Hegenberg <hegenberg@tixeltec.com>
2022-12-13 17:41:49 +0100

    Part on DatabaseClass finished

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
 require 'functions.php';
-
+require 'Database.php';
 //require 'router.php';
-// connect to MySQL database.
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
-$username = 'appUser';
-$password = 'password';
-$pdo = new PDO($dsn, $username, $password);
 
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
+$db = new Database();
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
