c96119c47f56279b070b32c33f410b014014502d
Patryk Hegenberg <hegenberg@tixeltec.com>
2022-12-14 08:31:40 +0100

    Section 2 finished.

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 require 'functions.php';
 require 'Database.php';
-require 'router.php';
+//require 'router.php';
 $username = 'appUser';
 $password = 'password';
 $config = require('config.php');
 $db = new Database($config['database'], $username, $password);
-$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+$id = $_GET['id'];
+// Variante 1
+$query = "SELECT * FROM posts WHERE id = ?";
+$posts = $db->query($query, [$id])->fetchAll(PDO::FETCH_ASSOC);
+// Variante 2
+//$query = "SELECT * FROM posts WHERE id = :id";
+//$posts = $db->query($query, [':id' => $id])->fetchAll(PDO::FETCH_ASSOC);
+// Beide Varianten machen inhaltlich das Gleiche.
+// freie Wahl, welche der beiden Varianten bevorzugt wird.
 
 dd($posts);
