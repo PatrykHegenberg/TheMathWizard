44a6d295d8a0e4259bf02dcd3f80d3d3a9298056
Patryk Hegenberg <hegenberg@tixeltec.com>
2022-12-18 15:08:59 +0100

    Part on authorisation finished

--- controllers/note.php
+++ controllers/note.php
@@ -1,11 +1,21 @@
 <?php
-    $username = 'appUser';
-    $password = 'password';
-    $config = require('config.php');
-    $db = new Database($config['database'], $username, $password);
+$username = 'appUser';
+$password = 'password';
+$config = require('config.php');
+$db = new Database($config['database'], $username, $password);
 
-    $heading = "Note";
+$heading = "Note";
 
-    $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']] )->fetch();
-    //dd($notes);
-    require "views/note.view.php";
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+])->fetch();
+
+if (!$note) {
+    abort();
+}
+$currentUserId = 1;
+if ($note['user_id'] != $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
+//dd($notes);
+require "views/note.view.php";
--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 require 'functions.php';
 require 'Database.php';
 require 'router.php';
-$username = 'appUser';
-$password = 'password';
-$config = require('config.php');
-$db = new Database($config['database'], $username, $password);
-$id = $_GET['id'];
+require 'Response.php';
+//$username = 'appUser';
+//$password = 'password';
+//$config = require('config.php');
+//$db = new Database($config['database'], $username, $password);
+//$id = $_GET['id'];
 // Variante 1
-$query = "SELECT * FROM posts WHERE id = ?";
-$posts = $db->query($query, [$id])->fetchAll(PDO::FETCH_ASSOC);
+//$query = "SELECT * FROM posts WHERE id = ?";
+//$posts = $db->query($query, [$id])->fetchAll(PDO::FETCH_ASSOC);
 // Variante 2
 //$query = "SELECT * FROM posts WHERE id = :id";
 //$posts = $db->query($query, [':id' => $id])->fetchAll(PDO::FETCH_ASSOC);
