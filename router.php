c0aeac263258bc820711abc582431a697f5824d5
Patryk Hegenberg <hegenberg@tixeltec.com>
2022-12-19 08:54:06 +0100

    Part on Forms finished

--- router.php
+++ router.php
@@ -1,16 +1,6 @@
 <?php
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-    '/learn' => 'controllers/learn.php',
-    '/mathe' => 'controllers/mathe.php',
-    '/note' => 'controllers/note.php',
-    '/notes' => 'controllers/notes.php',
-    '/addition' => 'controllers/addition.php'
-];
+$routes = require ('routes.php');
 
 function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
---
+++ routes.php
@@ -0,0 +1,12 @@
+<?php
+return[
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+    '/learn' => 'controllers/learn.php',
+    '/mathe' => 'controllers/mathe.php',
+    '/note' => 'controllers/note.php',
+    '/notes' => 'controllers/notes.php',
+    '/addition' => 'controllers/addition.php',
+    '/notes/create' => 'controllers/note-create.php'
+];
