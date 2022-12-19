a597943b45af63221fec55e2c905fff5021d7c33
Patryk Hegenberg <hegenberg@tixeltec.com>
2022-12-19 20:55:48 +0100

    Part on Escaping Chars finished

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,6 +1,11 @@
 <?php
 $heading = 'Create Note';
+$username = 'appUser';
+$password = 'password';
+$config = require('config.php');
+$db = new Database($config['database'], $username, $password);
+
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    dd($_POST);
+    $db->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)", ['body' => $_POST['body'], 'user_id' => 1]);
 }
 require 'views/note-create.view.php';
