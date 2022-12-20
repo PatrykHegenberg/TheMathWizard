ef902284397f50707335acb50fd5dd55365165b3
Patryk Hegenberg <hegenberg@tixeltec.com>
2022-12-20 08:29:19 +0100

    Part on Form validation finished

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -5,7 +5,21 @@ $password = 'password';
 $config = require('config.php');
 $db = new Database($config['database'], $username, $password);
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $db->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)", ['body' => $_POST['body'], 'user_id' => 1]);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errors = [];
+
+    if (strlen($_POST['body'] === 0)) {
+        $errors['body'] = 'A body is required.';
+    }
+    if (strlen($_POST['body'] > 1000)) {
+        $errors['body'] = 'The body cann not be more than 1000 characters.';
+    }
+    if (empty($errors)) {
+
+        $db->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)", [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 require 'views/note-create.view.php';
