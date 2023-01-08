cbebac47fa8840859bd6e46e50aa0e617519646a
Patryk Hegenberg <hegenberg@tixeltec.com>
2023-01-08 08:44:23 +0100

    added feature description to landing page and nes box style

--- views/index.view.php
+++ views/index.view.php
@@ -37,7 +37,8 @@
   
 </div>
 <div class="bg-white py-24 sm:py-32 lg:py-40">
-  <div class="mx-auto max-w-7xl px-6 lg:px-8 bg-gray-50">
+  <div class="nes-container with-title mx-auto max-w-7xl px-6 lg:px-8 bg-gray-50">
+    <p class="title">Features</p>
     <div class="sm:text-center">
       <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Der bessere Weg das 1x1 zu automatisieren.</p>
     </div>
@@ -53,7 +54,7 @@
           </div>
           <div class="sm:min-w-0 sm:flex-1">
             <p class="text-lg font-semibold leading-8 text-gray-900">Immer neue Level</p>
-            <p class="mt-2 text-base leading-7 text-gray-600">Durch zufällig generierte Level wird dir nie langweilig.</p>
+            <p class="mt-2 text-base leading-7 text-gray-600">Spannende, zufallsgenerierte Level im Dungeon-Crawler-Stil.</p>
           </div>
         </div>
 
@@ -69,6 +70,32 @@
             <p class="mt-2 text-base leading-7 text-gray-600">Die Aufgabe passen sich deinen aktuellen Fähigkeiten an.</p>
           </div>
         </div>
+        
+        <div class="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row">
+          <div class="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white sm:shrink-0">
+            <!-- Heroicon name: outline/scale -->
+            <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0012 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 01-2.031.352 5.988 5.988 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 01-2.031.352 5.989 5.989 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971z" />
+            </svg>
+          </div>
+          <div class="sm:min-w-0 sm:flex-1">
+            <p class="text-lg font-semibold leading-8 text-gray-900">Fokus auf das was zählt</p>
+            <p class="mt-2 text-base leading-7 text-gray-600">Fokus auf das Automatisieren der Multiplikation im Zahlenraum von 1 bis 10.</p>
+          </div>
+        </div>
+
+        <div class="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row">
+          <div class="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white sm:shrink-0">
+            <!-- Heroicon name: outline/scale -->
+            <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0012 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 01-2.031.352 5.988 5.988 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0l2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 01-2.031.352 5.989 5.989 0 01-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971z" />
+            </svg>
+          </div>
+          <div class="sm:min-w-0 sm:flex-1">
+            <p class="text-lg font-semibold leading-8 text-gray-900">Lernfortschritt immer im Blick.</p>
+            <p class="mt-2 text-base leading-7 text-gray-600">Lernziele und Fortschrittskontrollen, um den Fortschritt der Schüler zu verfolgen.</p>
+          </div>
+        </div> 
 
       </div>
     </div>
